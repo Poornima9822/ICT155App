8f760b2efe3d5a0f4d3872f95798af877c0c81bd
PoornimaRoopasinghe <poornimasbv@gmail.com>
2022-01-11 16:59:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+class StudentParent{
+        public $stdName;
+        public $stdRegNum;
+
+        public function __construct($stdName,$stdRegNum){
+            $this->stdName->$stdName;
+            $this->stdRegNum->$stdRegNum;
+        }
+    }
+    ?>
